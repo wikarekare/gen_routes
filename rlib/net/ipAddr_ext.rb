2ad6a2c2d63276a15b8cca014dc29bef9089025b
robertburrowes <r.burrowes@auckland.ac.nz>
2022-06-18 15:57:59 +1200

    rubocop and deploy

--- rlib/net/ipAddr_ext.rb
+++ rlib/net/ipAddr_ext.rb
@@ -1,61 +1,61 @@
 require 'ipaddr'
 
-#Additions to the standard IPAddr class.
-#I needed to be able to access the mask,
-#test if a network is a subnet of another network
-#Do simple additions to generate new IP addresses. 
+# Additions to the standard IPAddr class.
+# I needed to be able to access the mask,
+# test if a network is a subnet of another network
+# Do simple additions to generate new IP addresses.
 class IPAddr
   attr_accessor :addr
   attr_reader :mask_addr
 
-  #Expose mask in x.x.x.x format
+  # Expose mask in x.x.x.x format
   def mask_to_s
     _to_string(@mask_addr)
   end
-  
-  #Expose mask as integer
+
+  # Expose mask as integer
   def mask_to_i
     @mask_addr
   end
-  
-  #get mask in /n format, as integer. 
-  #Probably a better method, but this will do for now.
+
+  # get mask in /n format, as integer.
+  # Probably a better method, but this will do for now.
   def mask_bits
     mask_bits = 0
     shifted_mask = @mask_addr
-    (1..32).each do |i|
-      mask_bits += 1 if(shifted_mask&0x1) == 1
+    (1..32).each do |_i|
+      mask_bits += 1 if (shifted_mask & 0x1) == 1
       shifted_mask = shifted_mask >> 1
     end
     return mask_bits
   end
-  
+
   alias to_s_orig to_s
-  
+
   def to_s(with_bits: false)
     with_bits ? "#{_to_string(@addr)}/#{mask_bits}" : to_s_orig
   end
-  
+
   def net_include?(ipaddr)
     self.include?(ipaddr) && @mask_addr <= ipaddr.mask_to_i
   end
-  
-  #IP address addition, so we can iteration through a network
+
+  # IP address addition, so we can iteration through a network
   # @param value [Integer] is added to @addr and a new IPAddr is returned.
-  def +(value)
-    self.clone.set(@addr + value, @family)
+  def +(other)
+    self.clone.set(@addr + other, @family)
   end
-  
-  #Iterate through all IP addresses in this network
+
+  # Iterate through all IP addresses in this network
   def each
     ipaddr = self.clone.set(@addr, @family)
-    (0...(@mask_addr^0xffffffff)).each do |a|
+    (0...(@mask_addr ^ 0xffffffff)).each do |_a|
       yield(ipaddr)
       ipaddr.addr += 1
     end
   end
 
-  def ==(value)
-    @addr == value.addr && @mask_addr == value.mask_addr
+  def ==(other)
+    @addr == other.addr && @mask_addr == other.mask_addr
   end
 end
--- rlib/net/save_routes.rb
+++ rlib/net/save_routes.rb
@@ -1,4 +1,4 @@
-#save the node routes in files.
+# save the node routes in files.
 # @param fd [File] file we want to write to
 # @param @target [Symbol] One of :linux, :bsd, :zebra, :manual
 # @param @node_name [String] node we are saving routes for.
@@ -6,31 +6,33 @@
 #                      Route hash must have {:route => IPAddr, :network_name => String, :gw => String, :node_name => String, :local => Boolean}
 class Save_Routes
   def initialize(fd:, node_name:, target: )
-    @fd, @node_name, @target = fd, node_name, target
+    @fd = fd
+    @node_name = node_name
+    @target = target
     @route_count = 0
   end
-  
+
   def self.open( routes_dir:, node_name:, target: )
-    File.open(routes_dir + '/' + node_name, "w+") do |fd|
+    File.open(routes_dir + '/' + node_name, 'w+') do |fd|
       c = self.new(fd: fd, node_name: node_name, target: target)
       c.create_route_command_leadin
       yield c
       c.create_route_command_trailer
     end
   end
-  
+
   def create_route_command_leadin
     case @target
-    when :linux, :bsd 
-      @fd.puts "#!/bin/bash"
-      @fd.puts "# #{@node_name} #{Time.now.strftime("%Y-%m-%d %H:%M")} (#{@target})"
+    when :linux, :bsd
+      @fd.puts '#!/bin/bash'
+      @fd.puts "# #{@node_name} #{Time.now.strftime('%Y-%m-%d %H:%M')} (#{@target})"
     when :json
-      @fd.puts "{ ["
-      @fd.puts "// #{@node_name} #{Time.now.strftime("%Y-%m-%d %H:%M")} (#{@target})"
+      @fd.puts '{ ['
+      @fd.puts "// #{@node_name} #{Time.now.strftime('%Y-%m-%d %H:%M')} (#{@target})"
     when :zebra
-      @fd.puts "! #{@node_name} #{Time.now.strftime("%Y-%m-%d %H:%M")} (#{@target})"
+      @fd.puts "! #{@node_name} #{Time.now.strftime('%Y-%m-%d %H:%M')} (#{@target})"
     else
-      @fd.puts "# #{@node_name} #{Time.now.strftime("%Y-%m-%d %H:%M")} (#{@target})"
+      @fd.puts "# #{@node_name} #{Time.now.strftime('%Y-%m-%d %H:%M')} (#{@target})"
     end
   end
 
@@ -45,7 +47,7 @@ class Save_Routes
     case @target
     when :linux
       if route[:route].to_s(with_bits: true) == '0.0.0.0/0'
-        @fd.puts "# Default (Will already be defined on router.)"
+        @fd.puts '# Default (Will already be defined on router.)'
         @fd.puts "# /sbin/route add default #{route[:gw]}"
       else
         @fd.puts "# #{comment}"
@@ -53,19 +55,19 @@ class Save_Routes
       end
     when :bsd
       if route[:route].to_s(with_bits: true) == '0.0.0.0/0'
-        @fd.puts "# Default"
+        @fd.puts '# Default'
         @fd.puts "  /sbin/route add default #{route[:gw]}"
       else
         @fd.puts "# #{comment}"
         @fd.puts "  /sbin/route add -net #{route[:route].to_string} -netmask #{route[:route].mask_to_s} #{route[:gw]}"
       end
     when :ubnt_conf
-      if(route[:network_name] == 'default')
+      if route[:network_name] == 'default'
         @fd.puts "route.1.comment=#{comment}"
         @fd.puts "route.1.gateway=#{route[:gw]}"
-        @fd.puts "route.1.ip=0.0.0.0"
+        @fd.puts 'route.1.ip=0.0.0.0'
         @fd.puts "route.1.netmask=#{route[:route].mask_to_s}"
-        @fd.puts "route.1.status=enabled"
+        @fd.puts 'route.1.status=enabled'
       else
         @route_count = @route_count == 0 ? 2 : @route_count + 1
         @fd.puts "route.#{@route_count}.comment=#{comment}"
@@ -73,26 +75,26 @@ class Save_Routes
         @fd.puts "route.#{@route_count}.ip=#{route[:route].to_string}"
         @fd.puts "route.#{@route_count}.netmask=#{route[:route].mask_to_s}"
         @fd.puts "route.#{@route_count}.status=enabled"
-      end  
+      end
     when :zebra
       if route[:route].to_s(with_bits: true) == '0.0.0.0/0'
-        @fd.puts "! Default"
+        @fd.puts '! Default'
         @fd.puts "ip route 0.0.0.0/0 #{route[:gw]}"
       else
         @fd.puts "! #{comment}"
         @fd.puts "ip route #{route[:route].to_s(with_bits: true)} #{route[:gw]}"
       end
     when :json
-      if @route_count != 0
-        @fd.puts ',' 
-      else
+      if @route_count == 0
         @route_count += 1
+      else
+        @fd.puts ','
       end
       @fd.puts "// #{comment}"
       @fd.print "  { \"route\": \"#{route[:route].to_string}\", \"netmask\": \"#{route[:route].mask_to_s}\", \"gw\": \"#{route[:gw]}\"}"
-    else #don't have a format for this, so make something up
+    else # don't have a format for this, so make something up
       if route[:route].to_s(with_bits: true) == '0.0.0.0/0'
-        @fd.puts "# Default"
+        @fd.puts '# Default'
         @fd.puts "  #{route[:gw]}"
       else
         @fd.puts "# #{comment}"
@@ -102,11 +104,12 @@ class Save_Routes
   end
 
   def save_routes(routes:)
-    return if routes == nil
+    return if routes.nil?
 
     res = routes.sort_by { |v| v[:route].to_i }
     res.each do |route|
-      next if route[:local] #Don't need to output local interface routes.
+      next if route[:local] # Don't need to output local interface routes.
+
       create_route_command(route: route, comment: "Network #{route[:network_name]}  via #{route[:node_name]} <--------")
     end
   end
