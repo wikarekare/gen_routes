00020379d087a806d9bac98e5ee558189159cf1b
robertburrowes <r.burrowes@auckland.ac.nz>
2023-10-19 15:14:25 +1300

    More explicit conf location

--- sbin/net/gen_routes.rb
+++ sbin/net/gen_routes.rb
@@ -6,12 +6,11 @@ RLIB = '/wikk/rlib' unless defined? RLIB
 require_relative "#{RLIB}/net/network_node_def.rb" # Should pull this from the database, so we don't maintain a separate copy.
 require_relative "#{RLIB}/net/save_routes.rb"
 
-@conf = JSON.parse(File.read(__dir__ + '/conf/conf.json'))
-@auth = JSON.parse(File.read(__dir__ + '/conf/auth.json'))
-@nodes = Nodes.new
+@conf = JSON.parse(File.read('/wikk/etc/net/gen_routes.json'))
+@nodes = Nodes.new(conf: @conf['node_file'])
 
 # Mkdir for the routes shell scripts. Relative to script directory, unless full path is given.
-routes_dir = @conf['route_dir'][0, 1] == '/' ? @conf['route_dir'] : __dir__ + '/' + @conf['route_dir']
+routes_dir = @conf['route_dir']
 FileUtils.mkdir_p(routes_dir)
 
 # For each node in the network, output a route script
